5dfe4a4f2344046d2c429ed89059cc02b2b25f62
Tomas Lööw <tomas.loow@next-tech.com>
2021-12-07 15:33:19 +0100

    Use gradient decent. Solves with only 24 position value checks

--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -3,50 +3,49 @@ package day7
 import DailyProblem
 import java.io.File
 import kotlin.math.absoluteValue
+import kotlin.math.min
 
 fun parseIntsOnOneLineFile(path: String): List<Long> {
     return File(path)
         .readLines()
         .single()
         .split(",")
-        .map{ it.toLong()}
+        .map { it.toLong() }
 }
 
-enum class MoveCost {
-    Linear,
-    Triangular
-}
+private fun List<Long>.findBestPosition(): Long {
+    /*
+    We know that the value as a function of a position is a convex function. So we can solve with a
+    kind of binary search where we divide the range in half and looks at *two* values near the middle. We
+    look at the difference between these to determine where on the convex curve we are and choose which direction
+    to divide in next.
+     */
+    var lowerBound = this.minOrNull()!!
+    var upperBound = this.maxOrNull()!!
+    var middlePointScore: Long
 
-private fun List<Long>.findBestPosition(costType: MoveCost): Long {
-    val lb = this.minOrNull()
-    val ub = this.maxOrNull()
-    val res = (lb!!..ub!!)
-        .map {
-            positionCandidate -> this.findPositionScore(positionCandidate, costType)
+    while (upperBound > lowerBound + 1) {
+        val middlePoint = (lowerBound + upperBound) / 2
+        middlePointScore = this.findPositionScore(middlePoint)
+        val neighbourOfMiddleScore = this.findPositionScore(middlePoint + 1)
+        when (neighbourOfMiddleScore.compareTo(middlePointScore)) {
+            0  -> return middlePointScore  // If they are equal both must be optimal
+            1  -> upperBound = middlePoint // The rightmost value was larger, we are to the right of the critical point
+            -1 -> lowerBound = middlePoint // The lefttmost value was larger, we are to the left of the critical point
         }
-        .windowed(2) // We can stop looking as soon as we hit a local minimum
-        .takeWhile { window ->
-            val (cost1, cost2) = window
-            cost1 > cost2
-        }.last()[1]
-
-    return res
+    }
+    return min(
+        this.findPositionScore(lowerBound),
+        this.findPositionScore(upperBound)
+    )
 }
 
-private fun List<Long>.findPositionScore(pos: Long, costType: MoveCost) : Long {
-    return when (costType) {
-        MoveCost.Linear ->
-            sumOf { crabPosition ->
-                (crabPosition - pos).absoluteValue
-            }
-        MoveCost.Triangular ->
-            sumOf { crabPosition ->
-                // Use the formula for arithmetic sum. 1+2+...+n = n*(n+1)/2
-                val distance = (crabPosition - pos).absoluteValue
-                distance * (distance + 1) / 2
-            }
+private fun List<Long>.findPositionScore(pos: Long): Long {
+    return sumOf { crabPosition ->
+        // Use the formula for arithmetic sum. 1+2+...+n = n*(n+1)/2
+        val distance = (crabPosition - pos).absoluteValue
+        distance * (distance + 1) / 2
     }
-
 }
 
 class Problem(override val inputFilePath: String) : DailyProblem {
@@ -54,12 +53,15 @@ class Problem(override val inputFilePath: String) : DailyProblem {
 
     override fun part1(): Long {
         val crabPositions = parseIntsOnOneLineFile(inputFilePath)
-        return crabPositions.findBestPosition(MoveCost.Linear)
+        // Solution for part 1 is just the mean of the positions
+        val pos = crabPositions.sorted()[crabPositions.size / 2]
+        return crabPositions.sumOf { (it - pos).absoluteValue }
     }
 
     override fun part2(): Long {
         val crabPositions = parseIntsOnOneLineFile(inputFilePath)
-        return crabPositions.findBestPosition(MoveCost.Triangular)
+        val bestPos = crabPositions.findBestPosition()
+        return bestPos
     }
 }
 
